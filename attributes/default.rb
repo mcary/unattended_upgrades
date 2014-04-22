982536b4dcccdf77b5a2f79abe20352b16a2effa
Adham Helal <ahelal@users.noreply.github.com>
2014-04-22 11:28:15 +0200

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,2 @@
-default[:unattended_upgrades][:upgrade_email] = "root@localhost"
-default[:unattended_upgrades][:auto_reboot] = false
+default['unattended_upgrades']['upgrade_email'] = 'root@localhost'
+default['unattended_upgrades']['auto_reboot']   = 'false'
