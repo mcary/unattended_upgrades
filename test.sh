7c0ac8985a2825a643bb22ae4bf840688399453a
Marcel M. Cary <marcel@oak.homeunix.org>
2014-06-24 22:36:36 -0700

    Test on multiple versions of Ubuntu with test coverage
    * Setup Vagrantfile to define three VMs, one per Ubuntu version
    * Adjust test criteria to also apply to 12.04 and 14.04
    * Document test coverage

--- test.sh
+++ test.sh
@@ -10,6 +10,12 @@ echo "APT::Periodic::Verbose 2;" > /etc/apt/apt.conf.d/verbose
 # Update every time; don't wait a day
 echo "APT::Periodic::Update-Package-Lists -1;" > /etc/apt/apt.conf.d/update_interval
 
+# For 14.04 VM, root mail doesn't go to vagrant by default
+if ! grep '^root:' /etc/aliases; then
+    echo 'root: vagrant' >> /etc/aliases
+    newaliases
+fi
+
 # These are specified as 1 in the default config, but need to be -1 to rerun
 #echo "APT::Periodic::Download-Upgradeable-Packages -1;" > /etc/apt/apt.conf.d/download_upgradable_interval # download every time
 #echo "APT::Periodic::Unattended-Upgrade -1;" > /etc/apt/apt.conf.d/55upgrade_interval # upgrade every time
@@ -44,7 +50,7 @@ check_presence () {
 
   echo "$descr"
   for expr in "$@"; do
-    if grep -q $flags "$expr" $inbox; then
+    if grep -qE $flags "$expr" $inbox; then
       echo "  Ok: $expr"
     else
       echo "  Er: $expr"
@@ -55,20 +61,16 @@ check_presence () {
 }
 
 check_presence "" "Important parts of email" \
-  "Subject: unattended-upgrades result for 'lucid64'" \
+  "Subject: unattended-upgrades result for '(lucid64|precise64|ubuntu-14)'" \
   "Unattended upgrade returned: True" \
-  "Packages that are upgraded:" \
+  "Packages that (were|are) upgraded:" \
   "Package installation log:" \
   "All upgrades installed" \
 
-check_presence "" "Not sure *all* of these are required..." \
+check_presence "" "Ensure at least *something* was installed" \
   "Preconfiguring packages" \
-  "Unpacking replacement" \
+  "Unpacking (replacement |.+ over )" \
   "Setting up" \
-
-check_presence "" "At least *something* was installed" \
-  "Packages that are upgraded: ..........." \
-
 
 # Only included with verbose level 3
 #check_presence "" "Counts" \
