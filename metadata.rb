f4b94c03615fa842a11d54b84e5cca1ecd009966
Marcel M. Cary <marcel@oak.homeunix.org>
2014-06-25 21:22:30 -0700

    Document auto_reboot attribute in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "marcel@oak.homeunix.org"
 license          "Apache 2.0"
 description      "Installs/Configures APT unattended_upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.0"
+version          "0.1.1"
 name             "unattended_upgrades"
 
 %w{ ubuntu debian }.each do |os|
@@ -14,3 +14,8 @@ attribute "unattended_upgrades/upgrade_email",
   :display_name => "Unattended Upgrade Email",
   :description => "Email to receive notifications regarding APT package upgrades",
   :default => "root@localhost"
+
+attribute "unattended_upgrades/auto_reboot",
+  :display_name => "Unattended Upgrade Auto-Reboot",
+  :description => "Whether to reboot unattended when suggested by package updates",
+  :default => "false"
