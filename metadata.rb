00ad64fc68bedc60233daf9b1df6ac6a21970b03
Marcel M. Cary <marcel@oak.homeunix.org>
2014-06-26 20:31:34 -0700

    Bump minor version to 0.2.0 for enable_upgrades flag

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "marcel@oak.homeunix.org"
 license          "Apache 2.0"
 description      "Installs/Configures APT unattended_upgrades"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.2"
+version          "0.2.0"
 name             "unattended_upgrades"
 
 %w{ ubuntu debian }.each do |os|
