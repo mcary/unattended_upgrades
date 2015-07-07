1c588048e1bb4ac622a606d49cf60649438e947f
Michael Burns <michael.burns@rackspace.com>
2015-07-07 02:18:36 -0700

    initial blacklist and template logic

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,4 @@
-default[:unattended_upgrades][:upgrade_email] = "root@localhost"
+default[:unattended_upgrades][:upgrade_email] = 'root@localhost'
 default[:unattended_upgrades][:auto_reboot] = false
 default[:unattended_upgrades][:enable_upgrades] = true
+default[:unattended_upgrades][:blacklist] = %w() # %w(vim libc6 libc6-dev libc6-i686)
